ed7a5e51a2052ab944fb6ddd799083392c498c49
Alpaca-zip <zip.lottestr@gmail.com>
2022-12-31 12:13:26 +0900

    add igvc_course

---
+++ orange_gazebo/launch/orange_igvc.launch.py
@@ -0,0 +1,25 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+  world = LaunchConfiguration('world', default=[FindPackageShare('orange_gazebo'), '/worlds/orange_igvc.world'])
+
+  return LaunchDescription([
+    IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+        os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')
+      ),
+      launch_arguments={'world': world}.items(),
+    ),
+
+    IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+        os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzclient.launch.py')
+      ),
+    ),
+  ])
