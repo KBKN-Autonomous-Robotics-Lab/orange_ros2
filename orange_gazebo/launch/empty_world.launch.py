1228b0827844a403c09b9f9a7e5cd6b542a7f622
Alpaca-zip <zip.lottestr@gmail.com>
2023-01-08 00:59:30 +0900

    add teleop feature

--- orange_gazebo/launch/empty_world.launch.py
+++ orange_gazebo/launch/empty_world.launch.py
@@ -1,8 +1,9 @@
 import os
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import Command
+from launch.substitutions import Command, LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
@@ -11,21 +12,27 @@ def generate_launch_description():
   world_file_name = 'empty.world'
   xacro_path = os.path.join(get_package_share_directory('orange_description'), 'xacro', xacro_file_name)
   world_path = os.path.join(get_package_share_directory('gazebo_ros'), 'worlds', world_file_name)
+  use_sim_time = LaunchConfiguration('use_sim_time', default = 'true')
 
   # Pose where we want to spawn the robot
   spawn_x = '0.0'
-  spawn_y = '0.0'
+  spawn_y = '0.1'
   spawn_z = '0.0'
   spawn_yaw = '0.0'
 
   return LaunchDescription([
+    DeclareLaunchArgument(
+      'use_sim_time',
+      default_value = 'true',
+      description = 'Use simulation (Gazebo) clock if true'
+    ),
 
     # gzserver
     IncludeLaunchDescription(
       PythonLaunchDescriptionSource(
         os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')
       ),
-      launch_arguments={'world': world_path}.items()
+      launch_arguments = {'world': world_path}.items()
     ),
 
     # gzclient
@@ -37,17 +44,17 @@ def generate_launch_description():
 
     # robot_state_publisher
     Node(
-      package='robot_state_publisher',
-      executable='robot_state_publisher',
-      output='screen',
-      parameters=[{'robot_description': Command(['xacro ', xacro_path])}]
+      package = 'robot_state_publisher',
+      executable = 'robot_state_publisher',
+      output = 'screen',
+      parameters=[{'use_sim_time': use_sim_time, 'robot_description': Command(['xacro ', xacro_path])}]
     ),
 
     # spawn_entity
     Node(
-      package='gazebo_ros',
-      executable='spawn_entity.py',
-      output='screen',
-      arguments=['-entity', 'my_test_robot', '-topic', '/robot_description', '-x', spawn_x, '-y', spawn_y, '-z', spawn_z, '-Y', spawn_yaw]
+      package = 'gazebo_ros',
+      executable = 'spawn_entity.py',
+      output = 'screen',
+      arguments = ['-entity', 'my_test_robot', '-topic', '/robot_description', '-x', spawn_x, '-y', spawn_y, '-z', spawn_z, '-Y', spawn_yaw]
     )
   ])
---
+++ orange_teleop/launch/teleop_joy.launch.py
@@ -0,0 +1,50 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+  config_file_name = 'elecom.yaml'
+  config_filepath_ = os.path.join(get_package_share_directory('orange_teleop'), 'config', config_file_name)
+  joy_dev = LaunchConfiguration('joy_dev', default = '/dev/input/js0')
+  config_filepath = LaunchConfiguration('config_filepath', default = config_filepath_)
+  cmd_vel = LaunchConfiguration('cmd_vel', default = '/cmd_vel')
+
+  return LaunchDescription([
+    DeclareLaunchArgument(
+      'joy_dev',
+      default_value = '/dev/input/js0',
+      description = 'Name of the joystick port'
+    ),
+
+    DeclareLaunchArgument(
+      'config_filepath',
+      default_value = config_filepath_,
+      description = 'Joystick config file path'
+    ),
+
+    DeclareLaunchArgument(
+      'cmd_vel',
+      default_value = '/cmd_vel',
+      description = 'Topic name for cmd_vel'
+    ),
+
+    # joy node
+    Node(
+      package = 'joy',
+      executable = 'joy_node',
+      output = 'screen',
+      parameters=[{'dev': joy_dev, 'deadzone': 0.3, 'autorepeat_rate': 20.0}]
+    ),
+
+    # teleop_twist_joy
+    Node(
+      package = 'teleop_twist_joy',
+      executable = 'teleop_node',
+      output = 'screen',
+      parameters=[config_filepath],
+      remappings={('/cmd_vel', cmd_vel)}
+    )
+  ])
