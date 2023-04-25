923ba5d9266cb62e489a8706668ba6896ee621f8
Alpaca-zip <zip.lottestr@gmail.com>
2023-04-25 16:07:56 +0900

    add motor_driver_node to orange_bringup

---
+++ orange_bringup/setup.py
@@ -0,0 +1,34 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'orange_bringup'
+
+setup(
+  name=package_name,
+  version='0.0.0',
+  packages=[package_name],
+  data_files=[
+    ('share/ament_index/resource_index/packages',
+      ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+    (os.path.join('share', package_name, 'config'), glob('config/*')),
+  ],
+  install_requires=['setuptools'],
+  zip_safe=True,
+  maintainer=['Shibuya', 'Kubota', 'Mori'],
+  maintainer_email=[
+    'shunki.shibuya.5v@stu.hosei.ac.jp',
+    'koki.kubota.8p@stu.hosei.ac.jp',
+    'kimihiro.mori.4k@stu.hosei.ac.jp'
+  ],
+  description='This project is to use orange robot with ROS2',
+  license='Apache License, Version 2.0',
+  tests_require=['pytest'],
+  entry_points={
+    'console_scripts': [
+      'motor_driver_node = orange_bringup.motor_driver_node:main'
+    ],
+  },
+)
