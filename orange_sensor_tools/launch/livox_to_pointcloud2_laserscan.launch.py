19e7cabef433e2f5dcd9cec30fbfad976594501e
kugurofu <tenten31569@icloud.com>
2024-05-29 13:05:51 +0000

    feature ground_segmentation

--- orange_sensor_tools/launch/livox_to_pointcloud2_laserscan.launch.py
+++ orange_sensor_tools/launch/livox_to_pointcloud2_laserscan.launch.py
@@ -14,12 +14,12 @@ def generate_launch_description():
                 'target_frame': '',
                 'transform_tolerance': 0.01,
                 'min_height': -0.2, # -20cm
-                'max_height': 0.2,  #  20cm
+                'max_height': 1.2,  #  120cm
                 'angle_min': -3.1415,  # - M_PI
                 'angle_max': 3.1415,  # M_PI
                 'angle_increment': 0.0174,  # M_PI * 2 / 360.0 = 1 degree
                 'scan_time': 0.1,# 10Hz
-                'range_min': 0.1, # 10cm
+                'range_min': 0.2, # 20cm
                 'range_max': 70.0, # 70m
                 'use_inf': True,
                 'inf_epsilon': 1.0
