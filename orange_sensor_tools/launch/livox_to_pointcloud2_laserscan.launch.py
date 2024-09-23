659ca9501a31f925780016738430a75a5b27b6a0
kugurofu <tenten31569@icloud.com>
2024-09-23 10:06:40 +0000

    feature/mid360

--- orange_sensor_tools/launch/livox_to_pointcloud2_laserscan.launch.py
+++ orange_sensor_tools/launch/livox_to_pointcloud2_laserscan.launch.py
@@ -8,18 +8,18 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-            remappings=[('cloud_in', '/livox_points/obstacle'),
+            remappings=[('cloud_in', '/converted_pointcloud2'),
                         ('scan', '/livox_scan')],
             parameters=[{
                 'target_frame': '',
                 'transform_tolerance': 0.01,
                 'min_height': -0.2, # -20cm
-                'max_height': 1.2,  #  120cm=bringup 20cm=simulation
+                'max_height': 0.7,  #  70cm=bringup 20cm=simulation
                 'angle_min': -3.1415,  # - M_PI
                 'angle_max': 3.1415,  # M_PI
                 'angle_increment': 0.0174,  # M_PI * 2 / 360.0 = 1 degree
                 'scan_time': 0.1,# 10Hz
-                'range_min': 0.2, # 20cm
+                'range_min': 0.4, # 20cm
                 'range_max': 70.0, # 70m
                 'use_inf': True,
                 'inf_epsilon': 1.0
