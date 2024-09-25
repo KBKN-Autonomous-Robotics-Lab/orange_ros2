fd3cb0372769549a860411459aa87ad1ab9c3b3e
kugurofu <kugurofu@users.noreply.github.com>
2024-09-25 08:14:06 +0000

    apply format

--- orange_sensor_tools/launch/livox_to_pointcloud2_laserscan.launch.py
+++ orange_sensor_tools/launch/livox_to_pointcloud2_laserscan.launch.py
@@ -1,8 +1,8 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+
 
 def generate_launch_description():
     return LaunchDescription([
@@ -13,14 +13,14 @@ def generate_launch_description():
             parameters=[{
                 'target_frame': '',
                 'transform_tolerance': 0.01,
-                'min_height': -0.2, # -20cm
-                'max_height': 0.7,  #  70cm=bringup 20cm=simulation
+                'min_height': -0.2,  # -20cm
+                'max_height': 0.7,  # 70cm=bringup 20cm=simulation
                 'angle_min': -3.1415,  # - M_PI
                 'angle_max': 3.1415,  # M_PI
                 'angle_increment': 0.0174,  # M_PI * 2 / 360.0 = 1 degree
-                'scan_time': 0.1,# 10Hz
-                'range_min': 0.4, # 20cm
-                'range_max': 70.0, # 70m
+                'scan_time': 0.1,  # 10Hz
+                'range_min': 0.4,  # 20cm
+                'range_max': 70.0,  # 70m
                 'use_inf': True,
                 'inf_epsilon': 1.0
             }],
