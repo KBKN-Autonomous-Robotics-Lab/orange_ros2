43fb9d46deac6d87d8870b90d78a217cc8f8301b
kengohozumi <kengo.hozumi.5n@stu.hosei.ac.jp>
2024-10-23 10:24:43 +0000

    fix get_moving_base_quat_ttyUSB py

--- orange_bringup/orange_bringup/get_movingbase_quat_ttyUSB.py
+++ orange_bringup/orange_bringup/get_movingbase_quat_ttyUSB.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 import math
-
 import rclpy
 import serial
 from rclpy.node import Node
 from sensor_msgs.msg import Imu
 from std_msgs.msg import Header
-
 
 class GPS_heading_Data(Node):
     def __init__(self):
@@ -24,7 +22,8 @@ class GPS_heading_Data(Node):
         self.movingbase_msg = Imu()
 
         self.timer = self.create_timer(1.0, self.movingbase_publish_msg)
-
+        self.count = 0
+        
         self.get_logger().info("Start get_movingbase_quat_ttyUSB node")
         self.get_logger().info("-------------------------")
 
@@ -35,24 +34,37 @@ class GPS_heading_Data(Node):
             self.get_logger().error(f"Serial error: {serialerror}")
             return None
 
-        initial_letters = b"$GNHDT"
+        initial_letters_outdoor = b"$GNHDT"
+        initial_letters_indoor = b"$GPHDT"
 
-        line = serial_port.readline()
-        #self.get_logger().info(f"line: {line}")
-        talker_ID = line.find(initial_letters)
-        if talker_ID != -1:
-            line = line[(talker_ID-1):]
-            gps_data = line.split(b",")
-            self.get_logger().info(f"gps_data: {gps_data}")
-            heading = float(gps_data[1])
-            if heading is None:
-                self.get_logger().error("not GPS heading data")
-                heading = 0
-                return heading
-        else:
-            heading = 0
-            return heading
-       
+        while(1):
+            line = serial_port.readline()
+            #self.get_logger().info(f"line: {line}")
+            talker_ID_indoor = line.find(initial_letters_indoor)
+            talker_ID_outdoor = line.find(initial_letters_outdoor)            
+            if talker_ID_indoor != -1:
+                #self.get_logger().info("GPHDT ok")
+                #line = line[(talker_ID_indoor-1):]
+                gps_data = line.split(b",")
+                #self.get_logger().info(f"gps_data: {gps_data}")
+                heading = float(gps_data[1])
+                if heading is None:
+                    self.get_logger().error("not GPS heading data")
+                    heading = 0
+                break
+            if talker_ID_outdoor != -1:
+                #self.get_logger().info("GNHDT ok")
+                #line = line[(talker_ID_outdoor-1):]
+                gps_data = line.split(b",")
+                #self.get_logger().info(f"gps_data: {gps_data}")
+                heading = float(gps_data[1])
+                if heading is None:
+                    self.get_logger().error("not GPS heading data")
+                    heading = 0
+                break          
+                
+        serial_port.close()
+
         return heading
 
     def quaternion_from_euler(self, roll, pitch, yaw):
@@ -72,9 +84,10 @@ class GPS_heading_Data(Node):
 
     def movingbase_publish_msg(self):
         real_heading = self.get_gps_heading(self.dev_name)
+        #self.get_logger().info(f"real_heading: {real_heading}")
         if real_heading is not None and real_heading != 0:
 
-            robotheading = real_heading[1] + 90
+            robotheading = real_heading + 90
             if robotheading >= 360:
                 robotheading -= 360
 
@@ -107,16 +120,21 @@ class GPS_heading_Data(Node):
             self.movingbase_msg.orientation.z = -q[3]  # -z
             self.movingbase_msg.orientation.w = q[0]
             self.movingbase_msg.orientation_covariance[0] = robotheading
-
             self.heading_pub.publish(self.movingbase_msg)
         else:
+            self.movingbase_msg.header.stamp = self.get_clock().now().to_msg()
+            self.movingbase_msg.header.frame_id = "imu_link"
+            self.movingbase_msg.orientation.x = 0.0
+            self.movingbase_msg.orientation.y = 0.0
+            self.movingbase_msg.orientation.z = 0.0
+            self.movingbase_msg.orientation.w = 0.0
+            self.heading_pub.publish(self.movingbase_msg)
             self.get_logger().error("!!!!-not movingbase data-!!!!")
-
 
 def main(args=None):
     rclpy.init(args=args)
     GPS_heading_Data_node = GPS_heading_Data()
-    rclpy.spin_once(GPS_heading_Data_node)
+    rclpy.spin(GPS_heading_Data_node)
     GPS_heading_Data_node.destroy_node()
     rclpy.shutdown()
 
